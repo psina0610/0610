020ffe9933ec7438b20c9f1dca9d3c63f0479b26
psina0610 <152833180+psina0610@users.noreply.github.com>
2023-12-04 08:48:46 +0500

    задача которая ищет  0

---
+++ kij.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("нельзя вводить буквы! )");
+
+int q = 0;
+
+int w = Convert.ToInt32(Console.ReadLine()) ;
+while (w != 0)
+    
+{
+    q += w;
+    w = Convert.ToInt32(Console.ReadLine())   ;
+}
+Console.WriteLine(q);
+
+
+
+
+
+
